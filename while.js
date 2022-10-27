d8504fe8aa744aca06f8ab1846ef4ae4c857b664
Damian Sebastian Silvera <116028887+Damiansebastians@users.noreply.github.com>
2022-10-27 13:09:18 +0200

    Add files via upload
    Intrucción: Se solicita la creacion de un bucle while para identificar si el número es PAR o IMPAR.
    Se seguirá ejecutando siempre y cuando el número ingresado sea distinto de cero.
    Si el número ingresado es cero, se indicará mediante alert que el dato no es válido, se saldrá del bucle y finalizara la ejecución.

---
+++ while.js
@@ -0,0 +1,17 @@
+
+    var numero=1;
+
+        while(numero!=0){
+            
+            var numero=window.prompt("Indique un número");
+
+                if(numero%2==0 && numero!=0){
+                    window.alert("El número es PAR");
+                }
+                else{
+                    if(numero!=0)
+                    window.alert("El número es IMPAR");
+                } 
+            }
+        window.alert("Dato no válido");
+        document.write("FIN DE LA EJECUCIÓN");
